a2184aca70f51b276c273ab7d4c841b79c7dcd31
Chen Wu <henrychenwu98@gmail.com>
2023-09-07 15:47:36 -0400

    Clean code from arxiv version

---
+++ model/lib/__init__.py
@@ -0,0 +1,2 @@
+# Codes in this directory are MODIFIED from the original code.
+# The modified codes may not work well independently.
---
+++ model/model_utils.py
@@ -0,0 +1,6 @@
+MAX_SAMPLE_SIZE = 4096
+
+
+def requires_grad(model, flag=True):
+    for p in model.parameters():
+        p.requires_grad = flag
